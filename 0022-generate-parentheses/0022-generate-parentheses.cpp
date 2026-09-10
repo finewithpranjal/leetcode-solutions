0c19727e92e72b0b2edf592ff91fa2c29aeec791
Pranjal Malviya <pranjalmalviya37@gmail.com>
2026-09-11 00:05:57 +0530

    Time: 0 ms (100%), Space: 13 MB (81.72%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void fun(int open , int close, int n, string &temp, vector<string> &res) {
+          if(open == n and close == n) {
+            res.push_back(temp);
+            return;
+          }
+
+          if (open < n) {
+            temp.push_back('(');
+            fun(open + 1, close, n, temp, res);
+            temp.pop_back();
+          }
+
+          if (close < open) {
+            temp.push_back(')');
+            fun(open, close+1, n, temp, res);
+            temp.pop_back();
+          }
+
+          return;
+    }
+    vector<string> generateParenthesis(int n) {
+        int o = 0, c = 0;
+        string s; vector<string>r;
+        fun(o, c, n, s, r );
+        return r;
+    }
+};
